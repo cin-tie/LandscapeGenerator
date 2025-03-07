a41170bec99ec3ad29219016a64fa4b1db8b3c65
cin.tie <csizegod@gmail.com>
2025-03-07 13:36:49 +0300

    Second commit (Finished project, changeable generation)

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -5,6 +5,8 @@ namespace LandscapeGenerator
         private System.ComponentModel.IContainer components = null;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnLoad;
 
         protected override void Dispose(bool disposing)
         {
@@ -18,6 +20,8 @@ namespace LandscapeGenerator
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.btnGenerate = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -45,11 +49,41 @@ namespace LandscapeGenerator
             this.btnGenerate.UseVisualStyleBackColor = false;
             this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
             // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.FromArgb(130, 130, 130); // Цвет фона кнопки
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat; // Плоский стиль
+            this.btnSave.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point); // Шрифт
+            this.btnSave.ForeColor = System.Drawing.Color.White; // Цвет текста
+            this.btnSave.Location = new System.Drawing.Point(392, 535);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(60, 30);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save to";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.SeveHeightMap);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.BackColor = System.Drawing.Color.FromArgb(130, 130, 130); // Цвет фона кнопки
+            this.btnLoad.FlatStyle = System.Windows.Forms.FlatStyle.Flat; // Плоский стиль
+            this.btnLoad.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point); // Шрифт
+            this.btnLoad.ForeColor = System.Drawing.Color.White; // Цвет текста
+            this.btnLoad.Location = new System.Drawing.Point(464, 535);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(60, 30);
+            this.btnLoad.TabIndex = 1;
+            this.btnLoad.Text = "Load from";
+            this.btnLoad.UseVisualStyleBackColor = false;
+            this.btnLoad.Click += new System.EventHandler(this.LoadHeightMap);
+            // 
             // MainForm
             // 
             this.BackColor = System.Drawing.Color.FromArgb(105, 105, 105); // Цвет фона формы
             this.ClientSize = new System.Drawing.Size(536, 582);
             this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnLoad);
             this.Controls.Add(this.pictureBox1);
             this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point); // Шрифт формы
             this.Name = "MainForm";
--- MainForm.cs
+++ MainForm.cs
@@ -20,21 +20,53 @@ namespace LandscapeGenerator
         {
             heightMap = new int[width, height];
             Random random = new Random();
+            
+            heightMap[0, 0] = random.Next(-256, 256);
 
-            for (int x = 0; x < width; x++)
+            for (int i = 1; i < width; i++)
             {
-                for (int y = 0; y < height; y++)
+                heightMap[0, i] = heightMap[0, i - 1] + random.Next(-16, 16);
+                heightMap[0, i] = Math.Clamp(heightMap[0, i], -256, 256);
+            }
+
+            for (int i = 1; i < height; i++)
+            {
+                heightMap[i, 0] = heightMap[i - 1, 0] + random.Next(-16, 16);
+                heightMap[i, 0] = Math.Clamp(heightMap[i, 0], -256, 256);
+            }
+
+            for (int i = 1; i < width; i++)
+            {
+                heightMap[height - 1, i] = heightMap[height - 1, i - 1] + random.Next(-16, 16);
+                heightMap[height - 1, i] = Math.Clamp(heightMap[height - 1, i], -256, 256);
+            }
+
+            for (int i = height - 2; i > 0; i--)
+            {
+                heightMap[i, width - 1] = heightMap[i + 1, width - 1] + random.Next(-16, 16);
+                heightMap[i, width - 1] = Math.Clamp(heightMap[i, width - 1], -256, 256); 
+            }
+
+            heightMap[0, 0] = (heightMap[0, 1] + heightMap[1, 0]) / 2;
+
+            for (int x = 1; x < width - 1; x++)
+            {
+                for (int y = 1; y < height - 1; y++)
                 {
-                    heightMap[x, y] = random.Next(0, 256);
+                    heightMap[x, y] = (
+                        heightMap[x - 1, y] +
+                        heightMap[x, y - 1] + 
+                        heightMap[x - 1, y - 1]) / 3 + random.Next(-16, 16);
+                    heightMap[x, y] = Math.Clamp(heightMap[x, y], -256, 256);
                 }
             }
 
-            SmoothHeightMap();
+            //SmoothHeightMap();
         }
 
         private void SmoothHeightMap()
         {
-            for (int k = 0; k < 20; k++){
+            /*for (int k = 0; k < 5; k++){
                 for (int x = 1; x < width - 1; x++)
                 {
                     for (int y = 1; y < height - 1; y++)
@@ -57,9 +89,35 @@ namespace LandscapeGenerator
             {
                 heightMap[i, 0] = heightMap[i, 1];
                 heightMap[i, width - 1] = heightMap[i, width - 2]; 
-            }
+            }*/
         }
 
+        private static Color GenerateColor(int height){
+            if(height < -128){
+                return InterpolateColor(Color.DarkBlue, Color.Blue, height, -256, -128);
+            }
+            if(height < 0){
+                return InterpolateColor(Color.Blue, Color.LightBlue, height, -128, 0);
+            }
+            if(height < 64){
+                return InterpolateColor(Color.LightGreen, Color.Green, height, 0, 64);
+            }
+            if(height < 128){
+                return InterpolateColor(Color.Green, Color.SaddleBrown, height, 64, 128);
+            }
+            if(height < 192){
+                return InterpolateColor(Color.SaddleBrown, Color.Gray, height, 128, 192);
+            }
+            return InterpolateColor(Color.LightGray, Color.White, height, 192, 256);
+        }
+
+        private static Color InterpolateColor(Color color1, Color color2, int value, int min, int max){
+            double ratio = (double)(value - min)/(max - min);
+            int r = (int)(color1.R + (color2.R - color1.R) * ratio);
+            int g = (int)(color1.G + (color2.G - color1.G) * ratio);
+            int b = (int)(color1.B + (color2.B - color1.B) * ratio);
+            return Color.FromArgb(r, g, b);
+        }
         private void DrawHeightMap()
         {
             Bitmap bitmap = new Bitmap(width, height);
@@ -69,7 +127,7 @@ namespace LandscapeGenerator
                 for (int y = 0; y < height; y++)
                 {
                     int heightValue = heightMap[x, y];
-                    Color color = Color.FromArgb(heightValue, heightValue, heightValue);
+                    Color color = GenerateColor(heightValue);
                     bitmap.SetPixel(x, y, color);
                 }
             }
@@ -82,5 +140,27 @@ namespace LandscapeGenerator
             GenerateHeightMap();
             DrawHeightMap();
         }
+
+        private void SeveHeightMap(object sender, EventArgs e) {
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog()){
+                saveFileDialog.Filter = "JPG Image|*.jpg";
+                if(saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    pictureBox1.Image.Save(saveFileDialog.FileName);
+                }
+            }
+        }
+
+        private void LoadHeightMap(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "JPG Image|*.jpg";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    pictureBox1.Image = Image.FromFile(openFileDialog.FileName);
+                }
+            }
+        }
     }
-}
+}   
